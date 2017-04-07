1d035a730413803d8809819a9668b1e3a2dc5dee
bertytobing <berty.c.l.tobing@gdplabs.id>
2017-04-07 06:33:44 +0000

    delete delete dependency spring cloud

--- student/src/main/java/com/student/model/entity/Student.java
+++ student/src/main/java/com/student/model/entity/Student.java
@@ -1,7 +1,5 @@
 package com.student.model.entity;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -13,26 +11,49 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "students", uniqueConstraints = @UniqueConstraint(columnNames = { "nik" }))
 public class Student {
 
-    @Id
-    @Column(columnDefinition = "char(36)", nullable = false)
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
-    @NotEmpty
-    private String id;
+	@Id
+	@Column(columnDefinition = "char(36)", nullable = false)
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid2")
+	@NotEmpty
+	private String id;
 
-    @Column(nullable = false)
-    @Length(max = 255)
-    @NotEmpty
-    private String name;
+	@Column(nullable = false)
+	@Length(max = 255)
+	@NotEmpty
+	private String name;
 
-    @Column(nullable = false)
-    @Length(max = 255)
-    @NotEmpty
-    private String nik;
+	@Column(nullable = false)
+	@Length(max = 255)
+	@NotEmpty
+	private String nik;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNik() {
+		return nik;
+	}
+
+	public void setNik(String nik) {
+		this.nik = nik;
+	}
+
 }
